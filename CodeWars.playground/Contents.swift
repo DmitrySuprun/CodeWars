2976425d1e212f0ef57922db958071767c7b5a97
DmitrySuprun <suprun1212@gmail.com>
2022-08-11 08:09:56 +0300

    Initial commit

---
+++ CodeWars.playground/Contents.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+func phone(_ strng: String, _ num: String) -> String {
+    let phoneNumber, name, address: String
+//    "Phone => num, Name => name, Address => adress"
+
+    
+    
+    
+    
+    print(address)
+    
+    
+    
+    return "result"
+}
+
+phone("first", "second")
