0032052eb8ab3a34657c1d669a694d919ca4b5f9
DmitrySuprun <suprun1212@gmail.com>
2022-08-11 08:11:00 +0300

    Start

--- CodeWars.playground/Contents.swift
+++ CodeWars.playground/Contents.swift
@@ -3,15 +3,6 @@ import Foundation
 func phone(_ strng: String, _ num: String) -> String {
     let phoneNumber, name, address: String
 //    "Phone => num, Name => name, Address => adress"
-
-    
-    
-    
-    
-    print(address)
-    
-    
-    
     return "result"
 }
 
