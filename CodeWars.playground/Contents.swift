86464c33ae18e01559d3101b9cf1dd15fc5a4f30
DmitrySuprun <suprun1212@gmail.com>
2022-08-16 14:42:56 +0300

    Next bigger number

--- CodeWars.playground/Contents.swift
+++ CodeWars.playground/Contents.swift
@@ -1,9 +1,30 @@
 import Foundation
 
-func phone(_ strng: String, _ num: String) -> String {
-    let phoneNumber, name, address: String
-//    "Phone => num, Name => name, Address => adress"
-    return "result"
+func nextBigger(num: Int) -> Int? {
+    
+    var arrayOfNum = String(num).compactMap { String($0) }
+    
+    if arrayOfNum.count == 1 {
+        return nil
+    }
+    
+    var array = [String]()
+    
+    array.append(arrayOfNum.removeLast())
+    
+    while arrayOfNum.count != 0 {
+        
+        array.append(arrayOfNum.removeLast())
+        
+        if array.last! < array[array.count - 2] {
+            let removeNum = array.firstIndex(where: { $0 > array.last! })!
+            arrayOfNum.append(array.remove(at: removeNum))
+            if Int(arrayOfNum.joined() + array.sorted(by: <).joined()) == num {
+                return nil
+            }
+            return Int(arrayOfNum.joined() + array.sorted(by: <).joined())
+        }
+    }
+   
+  return nil
 }
-
-phone("first", "second")
