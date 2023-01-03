fff9da6cb5c69651b39fea174e8cfaac233cd296
DmitrySuprun <suprun1212@gmail.com>
2023-01-03 19:41:56 +0300

    prime number

--- CodeWars.playground/Contents.swift
+++ CodeWars.playground/Contents.swift
@@ -1,13 +1,27 @@
 import Foundation
 
-func findOutlier(_ array: [Int]) -> Int {
-    let odd = array.filter { $0 % 2 == 0 }
-    if odd.count == 1 {
-        return odd[0]
-    } else {
-        return array.filter { $0 % 2 != 0}[0]
-    }
+extension Int {
+  func isPrime() -> Bool {
+      if self == 1 || self == 2 || self == 3 {
+          return true
+      }
+      let maxDivider = Int(sqrt(Double(self)))
+      for i in 2...maxDivider {
+          if self % i == 0 { return false }
+      }
+      return true
+  }
 }
 
-findOutlier([8, 2, 4, 4, 7])
+func getPrimes(from start: Int, to end: Int) -> [Int] {
+    let range = start > end ? end...start : start...end
+    var result = [Int]()
+    for i in range {
+        if i.isPrime() {
+            result.append(i)
+        }
+    }
+    return result
+}
+
 
