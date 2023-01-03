1e271e888741a85b6fdd87f9eda589cfca1992f1
DmitrySuprun <suprun1212@gmail.com>
2023-01-03 21:10:24 +0300

    Duplicates

--- CodeWars.playground/Contents.swift
+++ CodeWars.playground/Contents.swift
@@ -1,27 +1,16 @@
 import Foundation
 
-extension Int {
-  func isPrime() -> Bool {
-      if self == 1 || self == 2 || self == 3 {
-          return true
-      }
-      let maxDivider = Int(sqrt(Double(self)))
-      for i in 2...maxDivider {
-          if self % i == 0 { return false }
-      }
-      return true
-  }
-}
-
-func getPrimes(from start: Int, to end: Int) -> [Int] {
-    let range = start > end ? end...start : start...end
-    var result = [Int]()
-    for i in range {
-        if i.isPrime() {
-            result.append(i)
+func countDuplicates(_ s:String) -> Int {
+    var result = 0
+    var text = s.lowercased()
+    while text.count != 0 {
+        let char = text.removeFirst()
+        if text.contains(where: { $0 == char }) {
+            result += 1
+            text.removeAll(where: { $0 == char })
         }
     }
     return result
 }
 
-
+countDuplicates("asdfas  awefasdfasdc")
